b85545ac2364b247e52b7e8ddb5196c043058989
bassam22h <bassamhamid22@gmail.com>
2025-04-07 19:34:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -102,7 +102,7 @@ def main():
             listen="0.0.0.0",
             port=int(os.getenv("PORT", 8443)),
             url_path=TOKEN,
-            webhook_url=f"https://social-bot.onrender.com/{TOKEN}"
+            webhook_url=f"https://social-bot-qmf3.onrender.com/{TOKEN}"
         )
     else:
         application.run_polling()
