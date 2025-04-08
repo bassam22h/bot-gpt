f593392773111dc8e40e34c39a0eb19b261019db
bassam22h <bassamhamid22@gmail.com>
2025-04-08 15:01:55 +0300

    Update config.py

--- project/config.py
+++ project/config.py
@@ -1,1 +1,7 @@
+import os
 
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")  # مثلاً: "@mychannel"
+CHANNEL_LINK = os.getenv("CHANNEL_LINK")          # مثلاً: "https://t.me/mychannel"
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
