681aeb9f40260ebd5bee27eb6258956cbc5a61a0
bassam22h <bassamhamid22@gmail.com>
2025-04-11 00:24:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,9 @@ OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
 CHANNEL_LINK = os.getenv("CHANNEL_LINK")
 
-# دعم عدة معرفات مشرف مفصولة بفواصل
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
+# تعديل الكود ليتجنب الخطأ إذا كانت القيمة فارغة
+admin_ids_str = os.getenv("ADMIN_IDS", "")
+if admin_ids_str:
+    ADMIN_IDS = [int(x) for x in admin_ids_str.split(",") if x.strip().isdigit()]
+else:
+    ADMIN_IDS = []
